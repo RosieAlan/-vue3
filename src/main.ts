25d00fdbcb0e7f185ecd6b87f8d35d0f64013762
“alan” <516308181@qq.com>
2025-07-07 22:18:06 +0800

    test husky2

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,7 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 const func = () => {
-
-          console.log("Hello World");
+  console.log("Hello World");
 };
 func();
 createApp(App).mount("#app");
