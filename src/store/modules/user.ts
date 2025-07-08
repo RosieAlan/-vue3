234c2b4630ca884d8178a96c2bf710dac8f57ec7
“alan” <516308181@qq.com>
2025-07-08 21:44:14 +0800

    登陆模块

--- src/store/modules/user.ts
+++ src/store/modules/user.ts
@@ -1,13 +1,10 @@
 //用户模块
 import { defineStore } from "pinia";
 //引入接口
-import { reqLogin,  } from "@/api/user";
-import type {
-  loginFormData,
-  loginResponseData,
-} from "@/api/user/type";
+import { reqLogin } from "@/api/user";
+import type { loginFormData, loginResponseData } from "@/api/user/type";
 import type { UserState } from "./types/type";
-import { SET_TOKEN, GET_TOKEN,} from "@/utils/token";
+import { SET_TOKEN, GET_TOKEN } from "@/utils/token";
 const useUserStore = defineStore("User", {
   state: (): UserState => ({
     token: GET_TOKEN() || null,
